45fe7ea6c6200cb313d5a727bc44319259f6f869
Frank <frankocrospomaugaz@gmail.com>
2024-03-31 02:43:49 -0500

    saa

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
         vue({
@@ -17,16 +17,4 @@ export default defineConfig({
             },
         }),
     ],
-    build: {
-        // Especifica el directorio de salida para la producción
-        outDir: 'public/build',
-        // Limpiar el directorio de salida al construir el proyecto
-        emptyOutDir: true,
-        // Generar manifest.json en el build
-        manifest: true,
-        rollupOptions: {
-            // Especificar la entrada para facilitar la división de código si es necesario
-            input: 'resources/js/app.js',
-        },
-    },
 });
