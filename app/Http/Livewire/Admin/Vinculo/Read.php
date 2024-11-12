ed3a81e49c540d84e00c04b29013b759ce421443
Frank <115649785+FrankOcrospoma@users.noreply.github.com>
2024-11-12 11:16:26 -0500

    Update Read.php

--- app/Http/Livewire/Admin/Vinculo/Read.php
+++ app/Http/Livewire/Admin/Vinculo/Read.php
@@ -38,7 +38,7 @@ class Read extends Component
     {
         $data = Vinculo::where('vigencia', true); 
 
-        $instance = getCrudConfig('Vinculo');
+        $instance = getCrudConfig('vinculo');
         if($instance->searchable()){
             $array = (array) $instance->searchable();
             $data->where(function (Builder $query) use ($array){
